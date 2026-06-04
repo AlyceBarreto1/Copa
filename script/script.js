5da5f9a0511e0c34a2f9066154aad50ba7daf4af
AlyceBarreto1 <alycinhapbyf@gmail.com>
2026-06-04 19:53:48 -0300

    feat: função de trocar cores no javascript

--- script/script.js
+++ script/script.js
@@ -1,6 +1,33 @@
 const textos = document.getElementsByClassName("h2");
+const tema = document.getElementById("trocar")
 
-textos[0].style.color = "green";
+let temaEscuro = false;
 
-textos[1].style.color = "green";
+tema.addEventListener("click",
+    function(){
 
+        if (temaEscuro === false){
+
+            document.body.style.background = "#18191a";
+
+            document.body.style.color = "white";
+
+            textos[0].style.color = "#f8f9fa";
+
+            textos[1].style.color = "#f8f9fa";
+            
+            temaEscuro = true;
+        }
+        else {
+            document.body.style.background = "#C5DAFF";
+
+            document.body.style.color = "black";
+
+            textos[0].style.color = "rgb(0, 0, 71)";
+
+            textos[1].style.color = "rgb(0, 0, 71)";
+
+            temaEscuro = false;
+        }
+    }
+);
