3480a7493031f9f67b83f5b734f0b457902d4079
AlyceBarreto1 <alycinhapbyf@gmail.com>
2026-06-04 20:28:41 -0300

    feat: todas as funções javascript

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 const textos = document.getElementsByClassName("h2");
-const tema = document.getElementById("trocar")
+const tema = document.getElementById("trocar");
+const cantores = document.querySelector(".cantores");
+const p = document.querySelectorAll("p");
+
+
+p[1].style.color = "pink";
+p[4].style.color = "purple";
 
 let temaEscuro = false;
 
@@ -15,7 +21,16 @@ tema.addEventListener("click",
             textos[0].style.color = "#f8f9fa";
 
             textos[1].style.color = "#f8f9fa";
-            
+
+            cantores.innerHTML = "CANTORES DA COPA";
+
+            cantores.style.color = "white";
+
+            p[0].style.color = "white";
+            p[2].style.color = "white";
+            p[3].style.color = "white";
+            p[5].style.color = "white";
+
             temaEscuro = true;
         }
         else {
@@ -26,6 +41,13 @@ tema.addEventListener("click",
             textos[0].style.color = "rgb(0, 0, 71)";
 
             textos[1].style.color = "rgb(0, 0, 71)";
+
+            cantores.style.color = "rgb(0, 0, 71)"
+
+            p[0].style.color = "#000047";
+            p[2].style.color = "#000047";
+            p[3].style.color = "#000047";
+            p[5].style.color = "#000047";
 
             temaEscuro = false;
         }
